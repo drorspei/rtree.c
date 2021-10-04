ed934b3b26471e206b4e5c379226f7024bf4b18a
Amit Sternschuss <amit.sternschuss@pagaya.com>
2021-10-04 14:01:24 +0000

    remove rtree.py and add setup.py file

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, Extension
+from setuptools.command.install import install
+import subprocess
+import os
+
+class CustomInstall(install):
+    def run(self):
+        command = "git clone https://github.com/drorspei/rtree.c"
+        process = subprocess.Popen(command, shell=True, cwd=".")
+        process.wait()
+        install.run(self)
+
+module = Extension('rtreecpy',
+                   sources = ['rtree.cpp', 'batch_search.cpp'],
+                   include_dirs = ['.'],
+                   extra_compile_args=['-fPIC', '-O3', '-shared', '-fpermissive'])
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="rtreecpy",
+    version="1.0.0",
+    author="dror",
+    author_email="dror.mastershin@gmail.com",
+    description="rtree c python bindings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drorspei/rtree.c",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    cmdclass={'install': CustomInstall},
+    ext_modules=[module],
+)
+
+
